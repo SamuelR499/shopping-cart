eecf7af2b02871ecb3b2655ea1583bd0a8acf161
Samuelr499 <samuelribeiro2@hotmail.com>
2022-04-28 17:23:25 -0300

    Requisito 9

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -11,7 +11,7 @@ describe('2 - Teste a função fecthItem', () => {
   it(`Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada;
   `, async() => {
     await fetchItem('MLB1615760527');
-    expect(fetch).toReturn();
+    expect(fetch).toHaveBeenCalled();
   });
 
   test('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', async () => {
